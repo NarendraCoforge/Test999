fcef75c16cbe717f46ee35d12139de1c730a37d5
NarendraCoforge <117641146+NarendraCoforge@users.noreply.github.com>
2022-11-08 15:03:57 +0530

    narendr: new WSITestProduct class created pushing into remote

---
+++ src/main/java/com/example/product/WSITestProduct.java
@@ -0,0 +1,15 @@
+package com.example.product;
+
+public class WSITestProduct {
+
+   private String name;
+   private int id;
+
+    public WSITestProduct() {
+    }
+
+    public WSITestProduct(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+}
